1d12f8ac70817d04eba9b3102ef73329a27853be
JoChicau <JoBCB@users.noreply.github.com>
2021-10-27 22:40:15 +0100

    Add files via upload

--- choreo_scripts-hydra.js
+++ choreo_scripts-hydra.js
@@ -4,58 +4,101 @@
 <title>  choreo-graphic-coding </title>*/
 
 // HYDRA // 'cntr' + 'shift' + 'enter' — runs all the code
+// CTRL-Enter: run a line of code
+// ALT-Enter: run a block
 
 
-s0.initScreen()
-src(s0)
-// insert code here
-.out()
+//   __ _  ___   ___   __| |   _____   _____ _ __ (_)_ __   __ _ 
+//  / _` |/ _ \ / _ \ / _` |  / _ \ \ / / _ \ '_ \| | '_ \ / _` |
+// | (_| | (_) | (_) | (_| | |  __/\ V /  __/ | | | | | | | (_| |
+//  \__, |\___/ \___/ \__,_|  \___| \_/ \___|_| |_|_|_| |_|\__, |
+//  |___/                                                  |___/ 
 
-// simple smooth
-.scrollX(0, 0.01)
-.kaleid(4)
 
-// wild kaleid
-.scale(5)
-.rotate(0.1,1)
-.kaleid(25)
+// on hyper
+// figlet -f acrobatic  "is"
+// catwalk
+// isometric2
+
+
+src(o0)
+
+// RGB line > to b&w waves
+
+// shapes
+// solid([1,0,0],[0,1,0],[0,0,1],1)
+// shape(4,0.4).repeat(2,50) // trans squares
+// shape()  // triangle
+// shape(20,0.1,0.01) // circles
+//   .scale(() => Math.sin(time)*5) // circles
+//  .repeat(() => Math.sin(time)*3) // circles
+
+// shake it lines
+.add(shape(2).color(2,2,2).scale(0.006).rotate(0.000001)) // change values to waves or remove line
+// .scale(1).rotate(0.1,1).scrollY(1, 0.1)
+.modulate(noise(()=> a.fft[1]*10 +0.01).scale(5,0.1)) // change middle > wavy
+//  .modulate(noise(2,0)) // with circles
+
+// fractals
+//  .add(o0,0.9)
+//  .repeat (2.2)
+//.scrollX(0, 0.01)
+
+// smooth kaleid
+
+// .rotate(0.1,1) // speeds up
+// .scrollX(0, 0.01).kaleid(4)
+
+// wilder kaleid
+.scale(5).rotate(0.1,1).kaleid(25)
 
 // wilder
 pattern = () => osc(900, 0).kaleid(100).scale(1, 0.4)
 
-// simple shape trasition
-.diff(shape(6,1.1,0.01))
-.layer(shape(2).color(1,1,1,({time})=>Math.sin(time*2)))
-
-//scrolling
-.scrollX(1,0.1)
-.scrollY(10,0.1)
-.scrollY(0, ({time}) => Math.sin(time*0.05)*0.05 )
-
-// shake it / lines
-.modulate(noise(()=> a.fft[1]*10 +0.01).scale(5,0.1))
-.add(shape(2).color(2,2,2).scale(0.006).rotate(0.000001))
-.modulate(noise(()=> a.fft[1]*10 +0.01).scale(5,0.1))
-
-// more lines
-.add(shape(2).color(2,2,2).scale(0.006).rotate(0.000001))
-.modulate(noise(()=> a.fft[1]*10 +0.01).scale(5,0.1))
+.out()
 
 
-// image inverted
-.color(-1,-1,-1) 
-// basic colour changing
-solid([1,0,0],[0,1,0],[0,0,1],1)
+// screenshare
+// mask rgg
 
-// bouncing repeat
-.repeat(1,5)
-.repeatY( ({time})=>Math.sin(time/2)*50)
-// add below around and around
-.rotate(1,()=> a.fft[3]*1 +0.01)
-.rotate(0.2,0.9) // second value is speed
+s0.initScreen()
+
+src(s0)
+  .scrollX(0, ({time}) => Math.sin(time*0.05)*0.05 )
+  .layer(osc(30,0.1,2).mask(shape(4)))
+.out()
 
 
-// fast tempo
+// blend wavy
+s0.initScreen()
+
+src(s0)
+.scale(0.5)
+.modulateScale(osc(3,0.5),-0.6)
+.blend(s0)
+.out()
+
+// blend wavy 
+
+s0.initScreen()
+src(s0)
+
+.modulate(osc(150,0,15))
+.out(o1)
+
+src(o1)
+.modulateKaleid(voronoi(() => Math.sin(time)*3,0.1,0.01),() => Math.sin(time)*2)
+.scale(() => Math.sin(time)*3)
+.out(o2)
+
+render(o2)
+
+.blend(o0)
+out()
+
+
+
+// pixelate fast tempo
 s0.initScreen()
 src(s0)
   .scrollX(0, ({time}) => Math.sin(time*0.05)*0.05 )
@@ -66,7 +109,9 @@ src(s0)
 .out()
 
 
-// GOOGLE MAPS AND SEARCH
+
+// next tab 
+// BREATHING
 function breathe_in () {
 	document.querySelector("#widget-zoom-in").click();
 }
@@ -80,6 +125,33 @@ function breathe_out () {
 function matrix() {
 document.querySelector(".widget-scene-canvas").style.transform="matrix(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)";
 }
+
+
+
+// back to hydra
+
+// smooth ending
+voronoi(1,-0.1,10)
+.add(osc(1,0,1)).kaleid(50)
+.scale(1,1,2).out(o1)
+src(o1).mult(src(s0).modulateRotate(o1,10), -0.5)
+.out(o0)
+
+
+
+// ENDING back to RGB
+
+// image inverted
+.color(-1,-1,-1) 
+// basic colour changing
+solid([1,0,0],[0,1,0],[0,0,1],1)
+
+
+
+
+
+
+
 
 var delay="10"; //how many seconds
 var count='0';
@@ -162,3 +234,43 @@ src(s0)
 shape() // fractals
   .add(o0,0.9)
   .repeat (2.2)
+
+
+// simple shape trasition
+shape()
+.diff(shape(6,1.1,0.01))
+.layer(shape(2).color(1,1,1,({time})=>Math.sin(time*2)))
+.scrollY(0, ({time}) => Math.sin(time*0.05)*0.05 )
+
+.out()
+
+// bw shapes overlay
+s0.initScreen()
+
+src(s0)
+shape(1,1)
+  .mult(voronoi(10000,2) //micro or macro
+  .blend(o0).luma())
+  .add(shape(10,1,0.01)
+       .rotate(1,1).mult(voronoi(1000,1).luma())
+       .rotate(1.5)).scrollX([0.1,-0.0625,0.005,0.00001],0)
+  .scrollY([0.1,-0.0625,0.005,0.0001],0)
+.blend(s0)
+.out()
+
+// bouncing repeat
+.repeat(1,5)
+.repeatY( ({time})=>Math.sin(time/2)*50)
+// add below around and around
+.rotate(1,()=> a.fft[3]*1 +0.01)
+.rotate(0.2,0.9) // second value is speed
+
+// blend simple water
+
+osc(5,.1).modulate(noise(1),.22).diff(o0)
+    .modulateScrollY(osc(2).modulate(osc().rotate(),.11))
+  .scale(1).color(0.9,1.014,1)
+.blend(s0)
+.out()
+
+
